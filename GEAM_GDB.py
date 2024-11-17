3345ebd6de86667eab8e76ba640133133bd78c0f
fergrc89 <f.garcia@ieo.csic.es>
2024-11-17 23:11:22 +0100

    Refactor GEAM_GDB.py to improve data handling and visualization; updated file paths, converted GeoDataFrames to EPSG:3857, and adjusted data source keys for consistency.

--- GEAM_GDB.py
+++ GEAM_GDB.py
@@ -1,47 +1,62 @@
+import pandas as pd
+import geopandas as gpd
+from shapely import wkt
+from shapely.geometry import Point, Polygon
 from bokeh.io import output_file, save, show
 from bokeh.models import TapTool, CustomJS, HoverTool, WheelZoomTool
 from bokeh.plotting import figure
 from bokeh.models import ColumnDataSource, Div
 from bokeh.models.tiles import WMTSTileSource
 from bokeh.layouts import column
-from datetime import datetime
+
 
 # Load the data
-groundtruth_data = pd.read_excel('data/GROUNTRUTH_DB.xlsx')
-stations_data = pd.read_excel('data/STATIONS_DB.xlsx')
-uv_data = pd.read_excel('data/UV_DB.xlsx')
-drone_data = pd.read_excel('data/DRONE_DB.xlsx')
+groundtruth_df = pd.read_excel('/Users/fer/Documents/PYTHON/15_GEAM_GDB_GitHub/data/GROUNDTRUTH_DB.xlsx')
+groundtruth_gdf = gpd.GeoDataFrame(groundtruth_df, geometry=[Point(xy) for xy in zip(groundtruth_df['dwc:decimalLongitude'], groundtruth_df['dwc:decimalLatitude'])], crs='EPSG:4326')
 
+stations_df = pd.read_excel('/Users/fer/Documents/PYTHON/15_GEAM_GDB_GitHub/data/STATIONS_DB.xlsx')
+stations_gdf = gpd.GeoDataFrame(stations_df, geometry=[Point(xy) for xy in zip(stations_df['dwc:decimalLongitude'], stations_df['dwc:decimalLatitude'])], crs='EPSG:4326')
 
-# Prepare the current date for output file naming
-current_date = datetime.now().strftime('%Y%m%d')
+uv_df = pd.read_excel('/Users/fer/Documents/PYTHON/15_GEAM_GDB_GitHub/data/UV_DB.xlsx')
+uv_df['geometry'] = uv_df['dwc:footprintWKT'].apply(wkt.loads)  
+uv_gdf = gpd.GeoDataFrame(uv_df, geometry='geometry', crs="EPSG:4326")
+
+drone_df = pd.read_excel('/Users/fer/Documents/PYTHON/15_GEAM_GDB_GitHub/data/DRONE_DB.xlsx')
+drone_df['geometry'] = drone_df['dwc:footprintWKT'].apply(wkt.loads)  
+drone_gdf = gpd.GeoDataFrame(drone_df, geometry='geometry', crs="EPSG:4326")
+
+# Convert all GeoDataFrames to EPSG:3857
+groundtruth_gdf = groundtruth_gdf.to_crs(epsg=3857)
+drone_gdf = drone_gdf.to_crs(epsg=3857)
+uv_gdf = uv_gdf.to_crs(epsg=3857)
+stations_gdf = stations_gdf.to_crs(epsg=3857)
 
 # Prepare data sources for different types of data
 groundtruth_source = ColumnDataSource(data={
     'x': groundtruth_gdf.geometry.x,
     'y': groundtruth_gdf.geometry.y,
-    'occurrenceID': groundtruth_gdf['occurrenceID']
+    'occurrenceID': groundtruth_gdf['dwc:occurrenceID']
 })
 
 stations_source = ColumnDataSource(data={
     'x': stations_gdf.geometry.x,
     'y': stations_gdf.geometry.y,
-    'eventID': stations_gdf['eventID']
+    'locationID': stations_gdf['dwc:locationID']
 })
 
 # Extract xs and ys as lists of lists for underwater video data
-underwater_video_data = {
-    'xs': [list(geom.xy[0]) for geom in underwater_video_gdf.geometry],
-    'ys': [list(geom.xy[1]) for geom in underwater_video_gdf.geometry],
-    'eventID': underwater_video_gdf['eventID']
+uv_data = {
+    'xs': [list(geom.xy[0]) for geom in uv_gdf.geometry],
+    'ys': [list(geom.xy[1]) for geom in uv_gdf.geometry],
+    'eventID': uv_gdf['dwc:eventID']
 }
-underwater_video_source = ColumnDataSource(data=underwater_video_data)
+uv_source = ColumnDataSource(data=uv_data)
 
 # Prepare data for drone polygons
 drone_data = {
     'xs': [list(geom.exterior.coords.xy[0]) for geom in drone_gdf.geometry],
     'ys': [list(geom.exterior.coords.xy[1]) for geom in drone_gdf.geometry],
-    'eventID': drone_gdf['eventID']
+    'eventID': drone_gdf['dwc:eventID']
 }
 drone_source = ColumnDataSource(data=drone_data)
 
@@ -60,7 +75,7 @@ map_figure.add_tile(tile_source)
 map_figure.toolbar.active_scroll = map_figure.select_one(WheelZoomTool)
 
 # Plot the data points and lines
-underwater_video_renderer = map_figure.multi_line(xs='xs', ys='ys', source=underwater_video_source, line_width=3, color='blue', legend_label='Underwater video tracks')
+underwater_video_renderer = map_figure.multi_line(xs='xs', ys='ys', source=uv_source, line_width=3, color='blue', legend_label='Underwater video tracks')
 stations_renderer = map_figure.scatter('x', 'y', source=stations_source, size=7, color='red', legend_label='Sampling stations')
 groundtruth_renderer = map_figure.scatter('x', 'y', source=groundtruth_source, size=3, color='green', legend_label='Species presence/absence')
 drone_renderer = map_figure.patches('xs', 'ys', source=drone_source, fill_alpha=0.2, line_width=2, fill_color="grey", legend_label='Drone surveys')
@@ -72,7 +87,7 @@ map_figure.legend.location = 'top_left'
 map_figure.legend.click_policy = 'hide'
 
 # Add HoverTools for displaying eventID and occurrenceID
-hover_tool_event = HoverTool(renderers=[stations_renderer], tooltips=[("Event ID", "@eventID")])
+hover_tool_event = HoverTool(renderers=[stations_renderer], tooltips=[("Location ID", "@locationID")])
 hover_tool_occurrence = HoverTool(renderers=[groundtruth_renderer], tooltips=[("Occurrence ID", "@occurrenceID")])
 hover_tool_video = HoverTool(renderers=[underwater_video_renderer], tooltips=[("Event ID", "@eventID")])
 hover_tool_drone = HoverTool(renderers=[drone_renderer], tooltips=[("Event ID", "@eventID")])
@@ -93,7 +108,7 @@ main_title = Div(text="""
 
 # Layout setup with stretch_both for full screen use
 layout = column(main_title, map_figure, sizing_mode="stretch_both")
-output_file(f'/Users/fer/Documents/PYTHON/14_GEAM_GDB/{current_date}_GEAM_GDB.html', title="GEAM GEODATABASE")
+output_file('/Users/fer/Documents/PYTHON/15_GEAM_GDB_GitHub/docs/index.html', title="GEAM GEODATABASE")
 
 # Save and display
 save(layout)
