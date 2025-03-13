c5c2a6df645c68bff73f3fe42fdeeffe3e214632
fergrc89 <f.garcia@ieo.csic.es>
2025-03-14 00:28:00 +0100

    feat: add collaborators stations data and enhance map legend with new symbols

---
+++ docs/data/5_STATIONS_collaborators_table.js
@@ -0,0 +1,37 @@
+var stations_collaborators = [
+    {
+        "locationID": "SANTANDER_PEDRE\u00d1A_CANT_NANO_13",
+        "decimalLatitude": 43.446824,
+        "decimalLongitude": -3.771508,
+        "institutionCode": "IHCantabria",
+        "contact": "barbara.ondiviela@unican.es"
+    },
+    {
+        "locationID": "SANTANDER_PEDRE\u00d1A_CANT_ZOS_14",
+        "decimalLatitude": 43.44799,
+        "decimalLongitude": -3.77409,
+        "institutionCode": "IHCantabria",
+        "contact": "barbara.ondiviela@unican.es"
+    },
+    {
+        "locationID": "OKA_BUSTURIA_PV_NANO_15",
+        "decimalLatitude": 43.376,
+        "decimalLongitude": -2.68163,
+        "institutionCode": "AZTI",
+        "contact": "jgarmendia@azti.es"
+    },
+    {
+        "locationID": "OKA_BUSTURIA_PV_NANO_16",
+        "decimalLatitude": 43.37387,
+        "decimalLongitude": -2.68168,
+        "institutionCode": "AZTI",
+        "contact": "jgarmendia@azti.es"
+    },
+    {
+        "locationID": "OKA_IBARRANGELUA_PV_NANO_17",
+        "decimalLatitude": 43.39525,
+        "decimalLongitude": -2.68088,
+        "institutionCode": "AZTI",
+        "contact": "jgarmendia@azti.es"
+    }
+];
