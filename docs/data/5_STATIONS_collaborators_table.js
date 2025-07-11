529ac9e6572ca52d7afc110cce7756e2008581c9
fergrc89 <f.garcia@ieo.csic.es>
2025-07-11 11:19:15 +0200

    rew release: monitoring stations

--- docs/data/5_STATIONS_collaborators_table.js
+++ docs/data/5_STATIONS_collaborators_table.js
@@ -1,22 +1,22 @@
 var stations_collaborators = [
     {
-        "locationID": "SANTANDER_PEDRE\u00d1A_CANT_NANO_13",
+        "locationID": "CANT_NANO_13",
         "decimalLatitude": 43.446824,
         "decimalLongitude": -3.771508,
-        "institutionCode": "IHCantabria",
+        "institutionCode": "IHC",
         "datasetName": "BIODIV (MITECO-IEO-CSIC)",
         "contact": "barbara.ondiviela@unican.es"
     },
     {
-        "locationID": "SANTANDER_PEDRE\u00d1A_CANT_ZOS_14",
+        "locationID": "CANT_ZOS_14",
         "decimalLatitude": 43.44799,
         "decimalLongitude": -3.77409,
-        "institutionCode": "IHCantabria",
+        "institutionCode": "IHC",
         "datasetName": "BIODIV (MITECO-IEO-CSIC)",
         "contact": "barbara.ondiviela@unican.es"
     },
     {
-        "locationID": "OKA_BUSTURIA_PV_NANO_15",
+        "locationID": "PV_NANO_15",
         "decimalLatitude": 43.376,
         "decimalLongitude": -2.68163,
         "institutionCode": "AZTI",
@@ -24,7 +24,7 @@ var stations_collaborators = [
         "contact": "jgarmendia@azti.es"
     },
     {
-        "locationID": "OKA_BUSTURIA_PV_NANO_16",
+        "locationID": "PV_NANO_16",
         "decimalLatitude": 43.37387,
         "decimalLongitude": -2.68168,
         "institutionCode": "AZTI",
@@ -32,7 +32,7 @@ var stations_collaborators = [
         "contact": "jgarmendia@azti.es"
     },
     {
-        "locationID": "OKA_IBARRANGELUA_PV_NANO_17",
+        "locationID": "PV_NANO_17",
         "decimalLatitude": 43.39525,
         "decimalLongitude": -2.68088,
         "institutionCode": "AZTI",
